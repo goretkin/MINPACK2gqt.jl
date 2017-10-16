f677335e4078b96561d247665ca0b7b65af40625
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2017-10-16 13:55:51 -0400

    comment syntax

--- estsv.jl
+++ estsv.jl
@@ -1,63 +1,63 @@
 function estsv(n::Integer,r::AbstractArray{Float64,1},ldr::Integer,svmin::Float64,z::AbstractArray{Float64,1})
       #TODO assert size(r) == (ldr,n), size(z) == (n)
 
-c Minpack Copyright Notice (1999) University of Chicago.  All rights reserved
-c see CopyrightMINPACK.txt
-c
-c     **********
-c
-c     Subroutine destsv
-c
-c     Given an n by n upper triangular matrix R, this subroutine
-c     estimates the smallest singular value and the associated
-c     singular vector of R.
-c
-c     In the algorithm a vector e is selected so that the solution
-c     y to the system R'*y = e is large. The choice of sign for the
-c     components of e cause maximal local growth in the components
-c     of y as the forward substitution proceeds. The vector z is
-c     the solution of the system R*z = y, and the estimate svmin
-c     is norm(y)/norm(z) in the Euclidean norm.
-c
-c     The subroutine statement is
-c
-c       subroutine estsv(n,r,ldr,svmin,z)
-c
-c     where
-c
-c       n is an integer variable.
-c         On entry n is the order of R.
-c         On exit n is unchanged.
-c
-c       r is a double precision array of dimension (ldr,n)
-c         On entry the full upper triangle must contain the full
-c            upper triangle of the matrix R.
-c         On exit r is unchanged.
-c
-c       ldr is an integer variable.
-c         On entry ldr is the leading dimension of r.
-c         On exit ldr is unchanged.
-c
-c       svmin is a double precision variable.
-c         On entry svmin need not be specified.
-c         On exit svmin contains an estimate for the smallest
-c            singular value of R.
-c
-c       z is a double precision array of dimension n.
-c         On entry z need not be specified.
-c         On exit z contains a singular vector associated with the
-c            estimate svmin such that norm(R*z) = svmin and
-c            norm(z) = 1 in the Euclidean norm.
-c
-c     Subprograms called
-c
-c       Level 1 BLAS ... dasum, daxpy, dnrm2, dscal
-c
-c     MINPACK-2 Project. October 1993.
-c     Argonne National Laboratory
-c     Brett M. Averick and Jorge J. More'.
-c
-c     **********
+# Minpack Copyright Notice (1999) University of Chicago.  All rights reserved
+# see CopyrightMINPACK.txt
+#
+#     **********
+#
+#     Subroutine destsv
+#
+#     Given an n by n upper triangular matrix R, this subroutine
+#     estimates the smallest singular value and the associated
+#     singular vector of R.
+#
+#     In the algorithm a vector e is selected so that the solution
+#     y to the system R'*y = e is large. The choice of sign for the
+#     components of e cause maximal local growth in the components
+#     of y as the forward substitution proceeds. The vector z is
+#     the solution of the system R*z = y, and the estimate svmin
+#     is norm(y)/norm(z) in the Euclidean norm.
+#
+#     The subroutine statement is
+#
+#       subroutine estsv(n,r,ldr,svmin,z)
+#
+#     where
+#
+#       n is an integer variable.
+#         On entry n is the order of R.
+#         On exit n is unchanged.
+#
+#       r is a double precision array of dimension (ldr,n)
+#         On entry the full upper triangle must contain the full
+#            upper triangle of the matrix R.
+#         On exit r is unchanged.
+#
+#       ldr is an integer variable.
+#         On entry ldr is the leading dimension of r.
+#         On exit ldr is unchanged.
+#
+#       svmin is a double precision variable.
+#         On entry svmin need not be specified.
+#         On exit svmin contains an estimate for the smallest
+#            singular value of R.
+#
+#       z is a double precision array of dimension n.
+#         On entry z need not be specified.
+#         On exit z contains a singular vector associated with the
+#            estimate svmin such that norm(R*z) = svmin and
+#            norm(z) = 1 in the Euclidean norm.
+#
+#     Subprograms called
+#
+#       Level 1 BLAS ... dasum, daxpy, dnrm2, dscal
+#
+#     MINPACK-2 Project. October 1993.
+#     Argonne National Laboratory
+#     Brett M. Averick and Jorge J. More'.
+#
+#     **********
       double precision one, p01, zero
       parameter (zero=0.d0,p01=1.0d-2,one=1.0d0)
 
@@ -71,7 +71,7 @@ c     **********
          z(i) = zero
       end do
 
-c     This choice of e makes the algorithm scale invariant.
+#     This choice of e makes the algorithm scale invariant.
 
       e = abs(r(1,1))
       if (e .eq. zero) then
@@ -80,11 +80,11 @@ c     This choice of e makes the algorithm scale invariant.
          return
       end if
 
-c     Solve R'*y = e.
+#     Solve R'*y = e.
 
       do i = 1, n
 
-c        Scale y. The factor of 0.01 reduces the number of scalings.
+#        Scale y. The factor of 0.01 reduces the number of scalings.
 
          e = sign(e,-z(i))
          if (abs(e-z(i)) .gt. abs(r(i,i))) then
@@ -93,7 +93,7 @@ c        Scale y. The factor of 0.01 reduces the number of scalings.
             e = temp*e
          end if
 
-c        Determine the two possible choices of y(i).
+#        Determine the two possible choices of y(i).
 
          if (r(i,i) .eq. zero) then
             w = one
@@ -103,7 +103,7 @@ c        Determine the two possible choices of y(i).
             wm = -(e+z(i))/r(i,i)
          end if
 
-c        Choose y(i) based on the predicted value of y(j) for j > i.
+#        Choose y(i) based on the predicted value of y(j) for j > i.
 
          s = abs(e-z(i))
          sm = abs(e+z(i))
@@ -125,11 +125,11 @@ c        Choose y(i) based on the predicted value of y(j) for j > i.
 
       ynorm = dnrm2(n,z,1)
 
-c     Solve R*z = y.
+#     Solve R*z = y.
 
       do j = n, 1, -1
 
-c        Scale z.
+#        Scale z.
 
          if (abs(z(j)) .gt. abs(r(j,j))) then
             temp = min(p01,abs(r(j,j))/abs(z(j)))
@@ -146,7 +146,7 @@ c        Scale z.
 
       end do
 
-c     Compute svmin and normalize z.
+#     Compute svmin and normalize z.
 
       znorm = one/dnrm2(n,z,1)
       svmin = ynorm*znorm
