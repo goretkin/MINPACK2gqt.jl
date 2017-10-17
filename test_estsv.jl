120b11c4fc0b1c38c9ffaa4d7b24cfea46bd8e17
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2017-10-17 19:03:27 -0400

    time twice

--- test_estsv.jl
+++ test_estsv.jl
@@ -1,14 +1,20 @@
 include("minpack2.jl")
 using Minpack2
 
-r = rand(3,3)
+r = rand(300,300)
 R = full(UpperTriangular(r))
 
 (ldr, n) = size(r); z = Array{Float64}(n);
 svmin = 0.0
 
-@time Minpack2.estsv(n, r, ldr, convert(Ptr{Float64}, pointer_from_objref(svmin)), z)
+Profile.clear_malloc_data()
 
+@time Minpack2.estsv(n, r, ldr, convert(Ptr{Float64}, pointer_from_objref(svmin)), z)
+@time Minpack2.estsv(n, r, ldr, convert(Ptr{Float64}, pointer_from_objref(svmin)), z)
+@profile Minpack2.estsv(n, r, ldr, convert(Ptr{Float64}, pointer_from_objref(svmin)), z)
+
+
+@time _, S, V = svd(R)
 @time _, S, V = svd(R)
 svmin_j = S[end]
 z_j = -V[:,end]
