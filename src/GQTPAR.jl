f7f16dcac24858502cb9282ea0c2e0b1b3b9bd4e
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2017-10-27 23:51:54 -0400

    make non-allocating version of estsv
    the convenience versions

--- src/GQTPAR.jl
+++ src/GQTPAR.jl
@@ -1,4 +1,5 @@
 module GQTPAR
+  export GQTWorkspace, solve!
   include("fortran_help.jl")
   using .FortranHelp
   #import FortranHelp: @aref
@@ -12,11 +13,20 @@ module GQTPAR
 
   function estsv(R)
     n = size(R, 1)
-    ldr = stride(R,2)
-    svmin = Ref{Float64}()
     z = Array{Float64}(n)
+    return estsv!(R, z)
+  end
 
+  function estsv!(R, z)
+    svmin = Ref{Float64}()  # will heap-allocate on 0.6
+    return estsv!(R, z, svmin)
+  end
+
+  function estsv!(R, z, svmin)
+    n = size(R, 1)
+    ldr = stride(R,2)
     estsv(n, R, ldr, svmin, z)
     return (z, svmin[])
   end
+
 end
